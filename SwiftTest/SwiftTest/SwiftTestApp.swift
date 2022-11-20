11c5891717b0372dd3977a3799cdd36f0ffa4433
tixanxiannaodai <2561386427@qq.com>
2022-11-20 22:39:18 +0800

    创建测试项目

---
+++ SwiftTest/SwiftTest/SwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestApp.swift
+//  SwiftTest
+//
+//  Created by likai on 2022/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
